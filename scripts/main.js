6653e1f70fea807b84e710c7c2c8107f54d309a4
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2024-04-17 14:26:41 -0500

    tarea 8

--- scripts/main.js
+++ scripts/main.js
@@ -36,11 +36,14 @@ function mostrarEstadisticas(aprendiz) {
 }
 function mostrarCursosAprendiz(cursos) {
     var cursosTbody = document.createElement("tbody");
+    var estado = cursos.map(function (c) { return (c.calificacion > 60) ? 'green' : 'red'; });
+    var index = 0;
     for (var _i = 0, cursos_1 = cursos; _i < cursos_1.length; _i++) {
         var curso = cursos_1[_i];
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(curso.nombre, "</td>\n        <td>").concat(curso.horas, "</td>\n        <td>").concat(curso.calificacion, "</td>\n        <td>").concat(curso.certificado, "</td>\n        <td>").concat(curso.anio, "</td>\n        ");
+        trElement.innerHTML = "<td>".concat(curso.nombre, "</td>\n        <td>").concat(curso.horas, "</td>\n        <td style=\"color:").concat(estado[index], "\">").concat(curso.calificacion, "</td>\n        <td>").concat(curso.certificado, "</td>\n        <td>").concat(curso.anio, "</td>\n        ");
         cursosTbody.appendChild(trElement);
+        index++;
     }
     cursosTable.appendChild(cursosTbody);
 }
