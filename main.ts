86ced0bae4b402be4e023ad8effb48f8e4a67e9b
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2024-04-17 15:49:07 +0000

    tarea 4

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 import { NivelEducativo } from "./aprendiz.js";
 import { Aprendiz } from "./aprendiz.js";
 
-
-
 export const ap = new Aprendiz("Juan Pablo","Reyes Gomez","avatar.png",30,NivelEducativo.POSGRADO)
 
-console.log(ap);
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
+    let tbodyAprendiz=document.createElement("tbody");
+    tbodyAprendiz.innerHTML=`
+    <tr><td colspan=2></td><img src="./${aprendiz.avatar}" height="100"></tr>
+    <tr><td>Nombres:</td> <td>  ${aprendiz.nombres}</td></>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></>
+    <tr><td>Nivel educativo:</td><td>${aprendiz.nivelEducativo}</td></>
+    <tr><td>Edad:</td><td>${aprendiz.edad}</td></>
+    `
+    aprendizTable.appendChild(tbodyAprendiz);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 import { NivelEducativo } from "./aprendiz.js";
 import { Aprendiz } from "./aprendiz.js";
 export var ap = new Aprendiz("Juan Pablo", "Reyes Gomez", "avatar.png", 30, NivelEducativo.POSGRADO);
-console.log(ap);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "\n    <tr><td colspan=2></td><img src=\"./" + aprendiz.avatar + "\" height=\"100\"></tr>\n    <tr><td>Nombres:</td> <td>  " + aprendiz.nombres + "</td></>\n    <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td></>\n    <tr><td>Nivel educativo:</td><td>" + aprendiz.nivelEducativo + "</td></>\n    <tr><td>Edad:</td><td>" + aprendiz.edad + "</td></>\n    ";
+    aprendizTable.appendChild(tbodyAprendiz);
+}
